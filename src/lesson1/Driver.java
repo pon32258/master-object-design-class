49e5f57642774b4af44071aa118ac82e4e461709
pon32258 <pon1852@hotmail.com>
2020-09-24 21:17:25 +0700

    chore(): first commit

---
+++ src/lesson1/Driver.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class Driver {
+
+    public void driver(Vehicle raceCar){
+        raceCar.accelerate();
+    }
+}
---
+++ src/lesson1/Vehicle.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class Vehicle {
+
+    public void startEngine(){
+        System.out.println("Starting my Engine");
+    }
+
+    public void stopEngine(){
+        System.out.println("Stopping my engine");
+    }
+
+    public void accelerate(){
+        System.out.println("accelerateing myself...");
+    }
+}
